5ec79b9aae6f38afbc9413d92be69497ad0adf4a
Ben Petty <benjamin.s.petty@gmail.com>
2017-01-13 15:14:26 -0800

    routes include profile, login, logout, and register new account

--- learning_journal/routes.py
+++ learning_journal/routes.py
@@ -5,8 +5,15 @@
 def includeme(config):
     """Include me."""
     config.add_static_view('static', 'static', cache_max_age=3600)
+    # ------- PUBLIC
     config.add_route('list', '/')
     config.add_route('detail', '/journal/{id:\d+}')
+    config.add_route('profile', '/profile/{id:\d+}')
+    # ------- GET PERMISSION
+    config.add_route('login', '/login')
+    config.add_route('logout', '/logout')
+    config.add_route('register', '/register')
+    # ------- AUTHOR VIEWS
     config.add_route('create', '/journal/new-entry')
     config.add_route('update', '/journal/{id:\d+}/edit-entry')
     config.add_route('delete', '/journal/{id:\d+}/delete-entry')
