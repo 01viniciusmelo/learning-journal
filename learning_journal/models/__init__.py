681f9679022adbf1495f9eaae4ef2a1faab56499
Ben Petty <benjamin.s.petty@gmail.com>
2017-01-13 15:12:49 -0800

    .jentry_model is now .lj_models

--- learning_journal/models/__init__.py
+++ learning_journal/models/__init__.py
@@ -9,7 +9,9 @@ import zope.sqlalchemy
 
 # import or define all models here to ensure they are attached to the
 # Base.metadata prior to any initialization routines
-from .jentry_model import Jentry  # noqa
+from .lj_models import Jentry  # noqa
+from .lj_models import User  # noqa
+
 
 # run configure_mappers after defining all of the models to ensure
 # all relationships can be setup
