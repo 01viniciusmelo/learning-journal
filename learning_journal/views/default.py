571e3e0d7d7793f1256898d85bf2414b120b0e17
Ben Petty <benjamin.s.petty@gmail.com>
2017-01-17 13:13:01 -0800

    add permission views

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -9,14 +9,20 @@ from jinja2 import Markup
 from pyramid.response import Response
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
+from pyramid.security import remember, forget
 
 from sqlalchemy.exc import DBAPIError
 
-from ..models import Jentry
+from learning_journal.models import Jentry, User
+from learning_journal.security import check_credentials
 
 
-@view_config(route_name='list',
-             renderer='../templates/list.jinja2')
+# ============================= PUBLIC VIEWS ================================ #
+
+@view_config(
+    route_name="list",
+    renderer="../templates/list.jinja2",
+)
 def list_view(request):
     """Homepage view lists all existing journal entries."""
     try:
@@ -26,8 +32,10 @@ def list_view(request):
     return {'journal': query.all(), 'project': 'learning_journal'}
 
 
-@view_config(route_name="detail",
-             renderer="../templates/detail.jinja2")
+@view_config(
+    route_name="detail",
+    renderer="../templates/detail.jinja2",
+)
 def detail_view(request):
     """Detail view expands an individual entry."""
     jentry_id = int(request.matchdict["id"])
@@ -42,8 +50,69 @@ def detail_view(request):
     return {"jentry": jentry}
 
 
-@view_config(route_name="create",
-             renderer="../templates/create.jinja2")
+@view_config(
+    route_name="profile",
+    renderer="../templates/profile.jinja2",
+)
+def profile_view(request):
+    """Profile view displays a users profile info."""
+    user_id = int(request.matchdict["id"])
+    user = request.dbsession.query(User).get(user_id)
+    if not user:
+        return Response("Not Found", content_type='text/plain', status=404)
+    return {"user": user}
+
+
+# ===================== GET PERMISSION VIEWS ================================ #
+
+@view_config(
+    route_name="login",
+    renderer="../templates/login.jinja2",
+)
+def login_view(request):
+    """Login view."""
+    if request.method == "POST" and request.POST:
+        if check_credentials(request):
+            auth_head = remember(request, request.POST["username"])
+            return HTTPFound(request.route_url("list"), headers=auth_head)
+    return {}
+
+
+@view_config(
+    route_name="logout",
+)
+def logout_view(request):
+    """Logout view."""
+    empty_head = forget(request)
+    return HTTPFound(request.route_url('list'), headers=empty_head)
+
+
+@view_config(
+    route_name="register",
+    renderer="../templates/register.jinja2",
+)
+def register_view(request):
+    """Registration view."""
+    if request.method == "POST" and request.POST:
+        new_user = User(
+            username=request.POST["username"],
+            password=request.POST["password"],
+            firstname=request.POST["firstname"],
+            lastname=request.POST["lastname"],
+            email=request.POST["email"],
+        )
+        request.dbsession.add(new_user)
+        return HTTPFound(request.route_url("list"))
+    return {}
+
+
+# =============================== AUTHOR VIEWS ============================== #
+
+@view_config(
+    route_name="create",
+    renderer="../templates/create.jinja2",
+    permission="author",
+)
 def create_view(request):
     """Create view makes a new post."""
     if request.method == "POST":
@@ -63,8 +132,11 @@ def create_view(request):
     return {}
 
 
-@view_config(route_name="update",
-             renderer="../templates/update.jinja2")
+@view_config(
+    route_name="update",
+    renderer="../templates/update.jinja2",
+    permission="author",
+)
 def update_view(request):
     """Update view edits an existing entry."""
     jentry_id = int(request.matchdict["id"])
@@ -78,8 +150,11 @@ def update_view(request):
     return {"jentry": jentry}
 
 
-@view_config(route_name="delete",
-             renderer="../templates/delete.jinja2")
+@view_config(
+    route_name="delete",
+    renderer="../templates/delete.jinja2",
+    permission="author",
+)
 def delete_view(request):
     """Delete view shows a warning pre confirmation of an entry deletion."""
     jentry_id = int(request.matchdict["id"])
@@ -93,6 +168,8 @@ def delete_view(request):
 #     """Delete forever permanently removes an entry from the database."""
 #     return
 
+
+# =========================================================================== #
 
 db_err_msg = """\
 Pyramid is having a problem using your SQL database.  The problem
