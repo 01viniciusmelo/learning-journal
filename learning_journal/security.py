bb8f7862e49a14df09d2d4835141b1c13e8ddcf2
Ben Petty <benjamin.s.petty@gmail.com>
2017-01-17 13:15:07 -0800

    incl security.py and views

---
+++ learning_journal/security.py
@@ -0,0 +1,51 @@
+"""Security."""
+
+import os
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.security import Everyone, Authenticated
+from pyramid.security import Allow
+
+from passlib.apps import custom_app_context as pwd_context
+
+from learning_journal.models import User
+
+
+class MyRoot(object):
+    """Root object."""
+
+    def __init__(self, request):
+        """Root init."""
+        self.request = request
+
+    __acl__ = [
+        (Allow, Everyone, 'view'),
+        (Allow, Authenticated, 'author'),
+    ]
+
+
+def check_credentials(request):
+    """Check user credentials."""
+    if "username" in request.POST and "password" in request.POST:
+        username = request.POST["username"]
+        password = request.POST["password"]
+        query = request.dbsession.query(User)
+        the_user = query.filter(User.username == username).first()
+        if the_user:
+            if pwd_context.verify(password, the_user.password):
+                return True
+    return False
+
+
+def includeme(config):
+    """Security-related configuration."""
+    auth_secret = os.environ.get('AUTH_SECRET', 'itsaseekrit')
+    authn_policy = AuthTktAuthenticationPolicy(
+        secret=auth_secret,
+        hashalg='sha512'
+    )
+    config.set_authentication_policy(authn_policy)
+    authz_policy = ACLAuthorizationPolicy()
+    config.set_authorization_policy(authz_policy)
+    config.set_default_permission('view')
+    config.set_root_factory(MyRoot)
