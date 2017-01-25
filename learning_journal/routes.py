2192de44ffb32249d8591a0ad2f840dc04318e66
Ben Petty <benjamin.s.petty@gmail.com>
2017-01-25 09:10:31 -0800

    add profile route

--- learning_journal/routes.py
+++ learning_journal/routes.py
@@ -5,16 +5,18 @@
 def includeme(config):
     """Include me."""
     config.add_static_view('static', 'static', cache_max_age=3600)
-    # ------- PUBLIC
+    # ------- PUBLIC ----------------------------------------------------------
     config.add_route('list', '/')
     config.add_route('detail', '/journal/{id:\d+}')
-    config.add_route('profile', '/profile/{id:\d+}')
-    # ------- GET PERMISSION
+    config.add_route('profile', '/profile/{username:\d+}')
+    # ------- GET PERMISSION --------------------------------------------------
     config.add_route('login', '/login')
     config.add_route('logout', '/logout')
     config.add_route('register', '/register')
-    # ------- AUTHOR VIEWS
+    # ------- AUTHOR VIEWS ----------------------------------------------------
     config.add_route('create', '/journal/new-entry')
     config.add_route('update', '/journal/{id:\d+}/edit-entry')
     config.add_route('delete', '/journal/{id:\d+}/delete-entry')
     config.add_route('delete_forever', '/journal/{id:\d+}/delete-forever')
+    # ------- ADMIN VIEWS -----------------------------------------------------
+    config.add_route('users', '/users')
