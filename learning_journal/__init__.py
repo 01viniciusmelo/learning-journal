a8963041abb7f53f4d861eb37c6a512f667dacaa
Ben Petty <benjamin.s.petty@gmail.com>
2017-01-29 17:07:34 -0800

    heroku fix attempt

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -1,10 +1,14 @@
 """Initializes the Learning Journal."""
 
 from pyramid.config import Configurator
+import os
 
 
 def main(global_config, **settings):
     """Return a Pyramid WSGI application."""
+    # ---- THE FOLLOWING LINE SHOULD BE COMMENTED OUT ONLY DURING TESTING --- #
+    settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
+    # ----------------------------------------------------------------------- #
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
